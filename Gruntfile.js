4539204f88fe0234bd0c1f9cbe44f3e5021ce3a8
bpeng <bpeng@whittl.com>
2014-04-29 17:04:04 -0500

    version update

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     meta: {
       project: 'bootstrap-timepicker',
-      version: '0.2.3'
+      version: '0.2.6'
     },
     jasmine: {
       build: {
